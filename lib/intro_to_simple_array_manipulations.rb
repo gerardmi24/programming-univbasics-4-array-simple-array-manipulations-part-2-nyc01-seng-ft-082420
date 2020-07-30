6982154adb614b34de57c9590b5267b179367ca9
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-30 01:34:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,23 @@
-def method_name
+def using_concat(array, element)
+  array.concat(element)
+end
 
+def using_include(array, element)
+  array.include?(element)
+end
+
+def using_include(array, element)
+  array.include?(element)
+end
+
+def using_include(array, element)
+  array.include?(element)
+end
+
+def using_include(array, element)
+  array.include?(element)
+end
+
+def using_include(array, element)
+  array.include?(element)
 end
